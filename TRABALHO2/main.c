ce1e516c0c90cd5c7ca0f79c6beabd9ca92fca4c
anthonyfreitasborges <103280259+anthonyfreitasborges@users.noreply.github.com>
2025-04-16 15:16:38 -0300

    Update main.c

--- TRABALHO2/main.c
+++ TRABALHO2/main.c
@@ -6,9 +6,8 @@
 
 
 int verificarSenha(char senha[]){
-int ok=0;
     char verificar [11]="orbita2025";
-    for(int i=1;i!='/0';i++){
+    for(int i=1;i<11;i++){
         if(verificar[i]!=senha[i]){
             return 0;
         }
@@ -37,9 +36,58 @@ int functionlogin(int n1){
         }
     }
 }
+
+
+/*🎯 Objetivo
+Após autenticação, o programa deve exibir um menu de opções numéricas. O menu deve ser
+apresentado repetidamente até que o usuário escolha a opção de saída.
+📌 O que deve ser feito
+• O menu deve permanecer em execução até que o usuário escolha sair.
+• Usar estrutura do...while para repetir o menu.
+• Validar entradas e chamar funções específicas por opção.
+7
+*/
+int entrandoMenu(int escolhamenu){
+    /*Criar uma função que simula a queima progressiva de combustível, iniciando com
+1000 litros. A cada iteração, devem ser consumidos 50 litros e exibida a
+quantidade restante. Exibir mensagens específicas em momentos-chave, como ao
+atingir 500 litros ("Atingindo atmosfera superior") e 100 litros ("Quase em
+órbita").
+Desafio: tente controlar cada iteração para acontecer de 0,5 em 0,5 segundos.
+8
+ */
+    if(escolhamenu==2){
+        int inicio=1000;
+    }
+ 
+ 
+  return 0;
+};
 int main()
 {
-
-    printf("bora %d ",functionlogin(2));
+    int escolha, menuPonto=1;
+        functionlogin(2);
+        do{
+        printf("ESCOLHA UM DOS MENUS:\n");
+        printf("menu 1 \n menu 2(Queima de Combustível) \n menu 3(Cálculo de Trajetória) \n sair 4 \n");
+        scanf("%d",&escolha);
+        switch (escolha){
+        case 1:
+            entrandoMenu(1);
+             menuPonto=1;
+            break;
+        case 2:
+            entrandoMenu(2);
+             menuPonto=1;
+            break;
+        case 3:
+            entrandoMenu(3);
+            menuPonto=1;
+            break;
+        case 4:
+            menuPonto=3;
+            break;
+        }
+    }while(menuPonto<2);
     return 0;
 }
