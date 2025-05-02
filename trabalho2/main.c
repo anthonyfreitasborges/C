d56f451868f7e294f56c4a2c258f7a1480039359
anthonyfreitasborges <103280259+anthonyfreitasborges@users.noreply.github.com>
2025-05-02 18:58:13 -0300

    Update main.c

--- trabalho2/main.c
+++ trabalho2/main.c
@@ -19,7 +19,7 @@ int verificarSenha(char senha[]){
 
 int functionlogin(int n1){
     setlocale(LC_ALL, "Portuguese");
-
+  int acesso=0;
     char senha[11];
     int i1=-1,erro=0;
     printf("LOGIN\n");
@@ -30,6 +30,7 @@ int functionlogin(int n1){
         if( verificarSenha(senha)==1){
             printf("Login efetivado com sucesso boas-vindas\n");
             i1=1;
+        int acesso=2;
         }else{
         printf("Senha está errada, digite novamente\n");
         erro ++;
@@ -39,18 +40,8 @@ int functionlogin(int n1){
             }
         }
     }
+    return acesso;
 }
-
-
-/*🎯 Objetivo
-Após autenticação, o programa deve exibir um menu de opções numéricas. O menu deve ser
-apresentado repetidamente até que o usuário escolha a opção de saída.
-📌 O que deve ser feito
-• O menu deve permanecer em execução até que o usuário escolha sair.
-• Usar estrutura do...while para repetir o menu.
-• Validar entradas e chamar funções específicas por opção.
-7
-*/
 float entradaDistancia(){
        int distanciaUsuario,angulo;
         printf("digite uma distancia em km \n");
@@ -63,24 +54,56 @@ float operacaoDistancia(){
         return angulo;
 
 }
+ void gravidadedosplanetas(int planetas){
+     int planeta= planetas;
+                if(planeta ==1){
+                    printf("A gravidade do planeta de Marte: 3.7 m/s²");
+                    if(3.7>15){
+                        printf("Gravidade forte. Aumente o empuxo.");
+                    }else{
+                        printf("Gravidade moderada.");
+                    }
+                }
+                if(planeta ==2){
+                    printf("A gravidade do planeta de Júpiter: 24.8 m/s²");
+                    if(24.8>15){
+                        printf("Gravidade forte. Aumente o empuxo.");
+                    }else{
+                        printf("Gravidade moderada.");
+                    }
+                }
+                if(planeta ==3){
+                    printf("A gravidade do planeta de Saturno: 10.4 m/s²");
+                    if(10.4>15){
+                        printf("Gravidade forte. Aumente o empuxo.");
+                    }else{
+                        printf("Gravidade moderada.");
+                    }
+                }
+}
 int entrandoMenu(int escolhamenu){
     if(escolhamenu ==2){
-int inicio=1000;
-    for(int interacao=0;interacao<=inicio;interacao++){
+        int inicio=1000;
+            for(int interacao=0;interacao<=inicio;interacao++){
             inicio=inicio-50;
-        printf("interacao %d e %d litros\n",interacao,inicio);
-    if(inicio==500){
-        printf("Atingindo atmosfera superior");
-    }if(inicio==100){
-    printf("quase em orbita");}} 
+            printf("interacao %d e %d litros\n",interacao,inicio);
+                if(inicio==500){
+                printf("Atingindo atmosfera superior");
+                }
+                if(inicio==100){
+                printf("quase em orbita");}
+                }
     }
     if(escolhamenu==3){
-           printf(":: %f\n",entradaDistancia());
+            printf(":: %f\n",entradaDistancia());
             printf(":: %f\n",operacaoDistancia());
 
     }
     if(escolhamenu==4){
-        
+            int planeta;
+            printf("escolha um planeta (1-Marte, 2-Júpiter ou 3-Saturno) ");
+            scanf('%d',&planeta);
+            gravidadedosplanetas(planeta);
     }
   return 0;
 };
@@ -88,8 +111,8 @@ int main()
 
 {       setlocale(LC_ALL, "Portuguese");
     int escolha, menuPonto=1;
-        functionlogin(2);
-        do{
+        if(functionlogin(2)==2){
+            do{
         printf("ESCOLHA UM DOS MENUS:\n");
         printf("2. Simular Queima de Combustível \n 3. Calcular Ângulo de Trajetória \n 4. Calcular Gravidade em Planeta \n");
         printf("5. Registrar Missão \n 6. Ver Histórico de Missões \n 6. Calcular Energia para Emergência (Recursivo)\n");
@@ -109,11 +132,24 @@ int main()
             menuPonto=1;
             break;
         case 4:
+            entrandoMenu(4);
+            menuPonto=1;
+            break;
+        case 5:
+            entrandoMenu(5);
+            menuPonto=1;
+            break;
+        case 6:
+            entrandoMenu(3);
             menuPonto=3;
             break;
         }
-    }while(menuPonto<2);
+            }while(menuPonto<2);
 
+
+        }else{
+
+        printf("\n Login nao realizado");}
 
     return 0;
 }
